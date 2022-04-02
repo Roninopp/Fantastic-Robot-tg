d7708c49f34acf83cabcc0f47e0f4a5ad4f90a29
Roninopp <99870677+Roninopp@users.noreply.github.com>
2022-04-03 00:24:28 +0530

    Update logomaker.py

--- TGN/modules/logomaker.py
+++ TGN/modules/logomaker.py
@@ -31,11 +31,11 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
     fname2 = "Emli.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @TGN_RO_BOT")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @FANTASTICFIGHTERBOT")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @GodfatherSupport, {e}')
+   await event.reply(f'Error Report @Ronin_Fighters_Fd, {e}')
 
 
 
@@ -65,11 +65,11 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
     fname2 = "Emli.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @TGN_RO_BOT")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @FANTASTICFIGHTERBOT")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @GodfatherSupport, {e}')
+   await event.reply(f'Error Report @Ronin_Fighters_Fd, {e}')
 
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
