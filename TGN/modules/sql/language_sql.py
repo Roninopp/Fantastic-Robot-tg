4006cbd9f80afb3b8e3e9c1276f931c773333326
Yash-Sharma-1807 <98401272+Yash-Sharma-1807@users.noreply.github.com>
2022-04-28 00:01:18 +0530

    Update language_sql.py

--- TGN/modules/sql/language_sql.py
+++ TGN/modules/sql/language_sql.py
@@ -1,7 +1,7 @@
 import threading
 
 from sqlalchemy import Column, String, UnicodeText
-from EnmuBot.modules.sql import SESSION, BASE
+from TGN.modules.sql import SESSION, BASE
 
 
 class ChatLangs(BASE):
