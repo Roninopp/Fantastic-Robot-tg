4c97f53c8bf292169979e3339543d7f363fc5788
Yash-Sharma-1807 <98401272+Yash-Sharma-1807@users.noreply.github.com>
2022-04-28 12:17:06 +0530

    Update userinfo.py

--- TGN/modules/userinfo.py
+++ TGN/modules/userinfo.py
@@ -449,6 +449,14 @@ def stats(update, context):
     uptime = datetime.datetime.fromtimestamp(boot_time()).strftime("%Y-%m-%d %H:%M:%S")
     status = "*╒═══「 System Statistics 」*\n\n"
     status += "*➢ System Start time:* " + str(uptime) + "\n"
+    uname = platform.uname()
+    status += "*➢ System:* " + str(uname.system) + "\n"
+    status += "*➢ Node name:* " + escape_markdown(str(uname.node)) + "\n"
+    status += "*➢ Release:* " + escape_markdown(str(uname.release)) + "\n"
+    status += "*➢ Machine:* " + escape_markdown(str(uname.machine)) + "\n"
+    mem = virtual_memory()
+    cpu = cpu_percent()
+    disk = disk_usage("/")
     status += "*➢ CPU:* " + str(cpu) + " %\n"
     status += "*➢ RAM:* " + str(mem[2]) + " %\n"
     status += "*➢ Storage:* " + str(disk[3]) + " %\n\n"
