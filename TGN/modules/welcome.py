7e44120a6882bf5846810567a14bba68e5492451
Rin Okumura <101918125+Kanekiken099999@users.noreply.github.com>
2022-05-31 13:00:21 +0530

    Update welcome.py

--- TGN/modules/welcome.py
+++ TGN/modules/welcome.py
@@ -200,7 +200,7 @@ def new_member(update: Update, context: CallbackContext):  # sourcery no-metrics
                 update.effective_message.reply_text(
                     f"Welcome to {html.escape(chat.title)} my king.", reply_to_message_id=reply
                ) 
-               welcome_log = Owner
+               welcome_log = (
                     f"{html.escape(chat.title)}\n"
                     f"#USER_JOINED\n"
                     f"welcome My King just joined the chat"
