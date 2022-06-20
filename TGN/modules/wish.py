a59a22352c5137df91b520efc0f6ef9a43a06232
Roninopp <99870677+Roninopp@users.noreply.github.com>
2022-06-20 09:27:27 +0530

    Update wish.py

--- TGN/modules/wish.py
+++ TGN/modules/wish.py
@@ -8,9 +8,9 @@ async def wish(e):
  if e.is_reply:
          mm = random.randint(1,100)
          lol = await e.get_reply_message()
-         fire = "https://telegra.ph/file/a537738bfac3c85ab919d.jpg"
+         fire = "https://telegra.ph/file/fd0d24d1d156f355572aa.jpg"
          await neko.send_file(e.chat_id, fire,caption=f"**Hey [{e.sender.first_name}](tg://user?id={e.sender.id}), Your wish has been cast.💜**\n\n__chance of success {mm}%__", reply_to=lol)
  if not e.is_reply:
          mm = random.randint(1,100)
-         fire = "https://telegra.ph/file/a537738bfac3c85ab919d.jpg"
+         fire = "https://telegra.ph/file/fd0d24d1d156f355572aa.jpg"
          await neko.send_file(e.chat_id, fire,caption=f"**Hey [{e.sender.first_name}](tg://user?id={e.sender.id}), Your wish has been cast.💜**\n\n__chance of success {mm}%__", reply_to=e)
