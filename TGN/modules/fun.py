d2e681d069d04d6ee4b642f9fc9d8226a098bd95
Roninopp <99870677+Roninopp@users.noreply.github.com>
2022-07-26 21:34:08 +0530

    Update fun.py

--- TGN/modules/fun.py
+++ TGN/modules/fun.py
@@ -1,72 +1,162 @@
+
+
 import html
 import random
-import time
+import re
 
-import TGN.modules.fun_strings as fun_strings
-from TGN import dispatcher
-from TGN.modules.disable import DisableAbleCommandHandler
-from TGN.modules.helper_funcs.chat_status import is_user_admin
-from TGN.modules.helper_funcs.extraction import extract_user
-from telegram import ChatPermissions, ParseMode, Update
+import requests as r
+from telegram import MAX_MESSAGE_LENGTH, ParseMode, Update
 from telegram.error import BadRequest
-from telegram.ext import CallbackContext
+from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
+from telegram.utils.helpers import escape_markdown
 
-GIF_ID = "CgACAgQAAx0CSVUvGgAC7KpfWxMrgGyQs-GUUJgt-TSO8cOIDgACaAgAAlZD0VHT3Zynpr5nGxsE"
+import TGN.modules.fun_strings as fun
+from TGN import DEMONS, DRAGONS, dispatcher
+from TGN.modules.disable import DisableAbleCommandHandler, DisableAbleMessageHandler
+from TGN.modules.helper_funcs.alternate import typing_action
+from TGN.modules.helper_funcs.chat_status import (is_user_admin)
+from TGN.modules.helper_funcs.extraction import extract_user
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+GN_IMG= "https://te.legra.ph/file/4a8fa24a2ee4b3f2edd0c.mp4"
+DECIDE_IMG= "https://telegra.ph/file/313cfbc4056bd3b5cd995.jpg"
+JUDGE_IMG= "https://telegra.ph/file/a2ebfdd262def30ad42a7.jpg"
 
 
-def runs(update: Update, context: CallbackContext):
-    temp = random.choice(fun_strings.RUN_STRINGS)
-    if update.effective_user.id == 828959161:
-        temp = "Run everyone, they just dropped a bomb 💣💣"
-    update.effective_message.reply_text(temp)
-
-
-def sanitize(update: Update, context: CallbackContext):
+@run_async
+@typing_action
+def goodnight(update, context):
     message = update.effective_message
-    name = (
-        message.reply_to_message.from_user.first_name
-        if message.reply_to_message
-        else message.from_user.first_name
-    )
-    reply_animation = (
-        message.reply_to_message.reply_animation
-        if message.reply_to_message
-        else message.reply_animation
-    )
-    reply_animation(GIF_ID, caption=f"*Sanitizes {name}*")
+    first_name = update.effective_user.first_name
+    reply = f"*Hey {escape_markdown(first_name)} \nGood Night! 😴*"
+    message.reply_photo(GN_IMG,reply, parse_mode=ParseMode.MARKDOWN)
 
-
-def sanitize(update: Update, context: CallbackContext):
+GM_IMG= "https://te.legra.ph/file/4a8fa24a2ee4b3f2edd0c.mp4"
+@run_async
+@typing_action
+def goodmorning(update, context):
     message = update.effective_message
-    name = (
-        message.reply_to_message.from_user.first_name
-        if message.reply_to_message
-        else message.from_user.first_name
-    )
-    reply_animation = (
-        message.reply_to_message.reply_animation
-        if message.reply_to_message
-        else message.reply_animation
-    )
-    reply_animation(random.choice(fun_strings.GIFS), caption=f"*Sanitizes {name}*")
+    first_name = update.effective_user.first_name
+    reply = f"*Hey {escape_markdown(first_name)} \n Good Morning!☀*"
+    message.reply_photo(GM_IMG,reply, parse_mode=ParseMode.MARKDOWN)
+    
+ASD_IMG= "https://te.legra.ph/file/c18ab8dbde14611c9f22f.jpg"
+@run_async
+@typing_action
+def asadali(update, context):
+    message = update.effective_message
+    first_name = update.effective_user.first_name
+    disable_web_page_preview=True,
+    reply = f"✪ ʜɪ ɪᴛ ɪs ᴍᴇ Fantastic ᴏᴡɴᴇʀ\n────────────────────────\n✪ {escape_markdown(first_name)} ᴅᴏsᴛ 🥰 ʜᴏᴡ ᴀʀᴇ ʏᴏᴜ\n────────────────────────\n✪ ᴡᴀɴᴛ ᴛᴏ ᴋɴᴏᴡ ᴀʙᴏᴜᴛ ᴍᴇ ᴠɪsɪᴛ ᴛʜɪs [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://t.me/RoninXJin_updates) 🥰\n────────────────────────\n✪ ──『ᴛʜᴀɴᴋs  ғᴏʀ  ᴜsɪɴɢ』"
+    message.reply_photo(ASD_IMG,reply, parse_mode=ParseMode.MARKDOWN)
+    
+@run_async
+def gbun(update, context):
+    user = update.effective_user
+    chat = update.effective_chat
+
+    if update.effective_message.chat.type == "private":
+        return
+    if int(user.id) in DRAGONS or int(user.id) in DEMONS:
+        context.bot.sendMessage(chat.id, (random.choice(fun.GBUN)))
 
 
+@run_async
+def gbam(update, context):
+    user = update.effective_user
+    chat = update.effective_chat
+    bot, args = context.bot, context.args
+    message = update.effective_message
+
+    curr_user = html.escape(message.from_user.first_name)
+    user_id = extract_user(message, args)
+
+    if user_id:
+        gbam_user = bot.get_chat(user_id)
+        user1 = curr_user
+        user2 = html.escape(gbam_user.first_name)
+
+    else:
+        user1 = curr_user
+        user2 = bot.first_name
+
+    if update.effective_message.chat.type == "private":
+        return
+    if int(user.id) in DRAGONS or int(user.id) in DEMONS:
+        gbamm = fun.GBAM
+        reason = random.choice(fun.GBAM_REASON)
+        gbam = gbamm.format(user1=user1, user2=user2, chatid=chat.id, reason=reason)
+        context.bot.sendMessage(chat.id, gbam, parse_mode=ParseMode.HTML)
+
+    
+    
+@run_async
+def judge(update, context):
+    context.bot.sendChatAction(update.effective_chat.id, "typing") # Bot typing before send messages
+    message = update.effective_message
+    if message.reply_to_message:
+      message.reply_to_message.reply_text(random.choice(fun.JUDGE_HANDLER))
+    else:
+      message.reply_text(fun.JUDGE_STRINGS)
+      
+      
+@run_async
+def alexa(update, context):
+    context.bot.sendChatAction(update.effective_chat.id, "typing") # Bot typing before send messages
+    message = update.effective_message
+    if message.reply_to_message:
+      message.reply_to_message.reply_text(random.choice(fun.ALEXA_HANDLER))
+    else:
+      message.reply_text(fun.ALEXA_STRINGS)
+
+
+@run_async
+def decide(update, context):
+    context.bot.sendChatAction(update.effective_chat.id, "typing") # Bot typing before send messages
+    message = update.effective_message
+    if message.reply_to_message:
+      message.reply_to_message.reply_text(random.choice(fun.DECIDE_HANDLER))
+    else:
+      message.reply_text(fun.DECIDE_STRINGS)
+
+@run_async
+@typing_action
+def repo(update, context):
+    update.effective_message.reply_text(fun.REPO)
+  
+@run_async
+def insult(update, context):
+    context.bot.sendChatAction(update.effective_chat.id, "typing") # Bot typing before send messages
+    message = update.effective_message
+    if message.reply_to_message:
+      message.reply_to_message.reply_text(random.choice(fun.SFW_STRINGS))
+    else:
+      message.reply_text(random.choice(fun.SFW_STRINGS)) 
+    
+    
+@run_async
+def abuse(update, context):
+    context.bot.sendChatAction(update.effective_chat.id, "typing") # Bot typing before send messages
+    message = update.effective_message
+    if message.reply_to_message:
+      message.reply_to_message.reply_text(random.choice(fun.ABUSE_STRINGS))
+    else:
+      message.reply_text(random.choice(fun.ABUSE_STRINGS))
+
+    
+@run_async
 def slap(update: Update, context: CallbackContext):
     bot, args = context.bot, context.args
     message = update.effective_message
     chat = update.effective_chat
 
-    reply_text = (
-        message.reply_to_message.reply_text
-        if message.reply_to_message
-        else message.reply_text
-    )
+    reply_text = message.reply_to_message.reply_text if message.reply_to_message else message.reply_text
 
     curr_user = html.escape(message.from_user.first_name)
     user_id = extract_user(message, args)
 
     if user_id == bot.id:
-        temp = random.choice(fun_strings.SLAP_SAITAMA_TEMPLATES)
+        temp = random.choice(fun.SLAP_ALEXA_TEMPLATES)
 
         if isinstance(temp, list):
             if temp[2] == "tmute":
@@ -79,8 +169,7 @@ def slap(update: Update, context: CallbackContext):
                     chat.id,
                     message.from_user.id,
                     until_date=mutetime,
-                    permissions=ChatPermissions(can_send_messages=False),
-                )
+                    permissions=ChatPermissions(can_send_messages=False))
             reply_text(temp[0])
         else:
             reply_text(temp)
@@ -96,19 +185,32 @@ def slap(update: Update, context: CallbackContext):
         user1 = bot.first_name
         user2 = curr_user
 
-    temp = random.choice(fun_strings.SLAP_TEMPLATES)
-    item = random.choice(fun_strings.ITEMS)
-    hit = random.choice(fun_strings.HIT)
-    throw = random.choice(fun_strings.THROW)
+    temp = random.choice(fun.SLAP_TEMPLATES)
+    item = random.choice(fun.ITEMS)
+    hit = random.choice(fun.HIT)
+    throw = random.choice(fun.THROW)
 
-    if update.effective_user.id == 1096215023:
-        temp = "@NeoTheKitty scratches {user2}"
+    if update.effective_user.id == 1793699293:
+        temp = "Alexa Owner @DUSHMANxRONIN Slaps You Noob 😁 {user2}"
 
-    reply = temp.format(user1=user1, user2=user2, item=item, hits=hit, throws=throw)
+    reply = temp.format(
+        user1=user1, user2=user2, item=item, hits=hit, throws=throw)
 
-    reply_text(reply, parse_mode=ParseMode.HTML)
+    reply_text(reply, parse_mode=ParseMode.HTML)   
+    
+    
+@run_async
+@typing_action
+def truth(update, context):
+    update.effective_message.reply_text(random.choice(fun.TRUTH))
 
 
+@run_async
+@typing_action
+def dare(update, context):
+    update.effective_message.reply_text(random.choice(fun.DARE))
+ 
+@run_async
 def pat(update: Update, context: CallbackContext):
     bot = context.bot
     args = context.args
@@ -131,285 +233,65 @@ def pat(update: Update, context: CallbackContext):
     pat_type = random.choice(("Text", "Gif", "Sticker"))
     if pat_type == "Gif":
         try:
-            temp = random.choice(fun_strings.PAT_GIFS)
+            temp = random.choice(fun.PAT_GIFS)
             reply_to.reply_animation(temp)
         except BadRequest:
             pat_type = "Text"
 
     if pat_type == "Sticker":
         try:
-            temp = random.choice(fun_strings.PAT_STICKERS)
+            temp = random.choice(fun.PAT_STICKERS)
             reply_to.reply_sticker(temp)
         except BadRequest:
             pat_type = "Text"
 
     if pat_type == "Text":
-        temp = random.choice(fun_strings.PAT_TEMPLATES)
+        temp = random.choice(fun.PAT_TEMPLATES)
         reply = temp.format(user1=user1, user2=user2)
         reply_to.reply_text(reply, parse_mode=ParseMode.HTML)
+       
+    
+GOODMORNING_HANDLER = DisableAbleMessageHandler(Filters.regex(r"(?i)(goodmorning|good morning)"), goodmorning, friendly="goodmorning")
+ASADALI_HANDLER = DisableAbleMessageHandler(Filters.regex(r"(?i)(asadali|asad)"), asadali, friendly="asadali")
+GOODNIGHT_HANDLER = DisableAbleMessageHandler(Filters.regex(r"(?i)(goodnight|good night)"), goodnight, friendly="goodnight")
+DECIDE_HANDLER = DisableAbleCommandHandler("decide", decide)
+
+REPO_HANDLER = DisableAbleCommandHandler("repo", repo)
+
+GBUN_HANDLER = CommandHandler("gbun", gbun)
+PAT_HANDLER = DisableAbleCommandHandler("pat", pat)
+GBAM_HANDLER = CommandHandler("gbam", gbam)
+DARE_HANDLER = DisableAbleCommandHandler("dare", dare)
+TRUTH_HANDLER = DisableAbleCommandHandler("truth", truth)
+INSULT_HANDLER = DisableAbleCommandHandler("insult", insult)
+ABUSE_HANDLER = DisableAbleCommandHandler("abuse", abuse)
+JUDGE_HANDLER = DisableAbleCommandHandler("judge", judge)
+DECIDE_HANDLER = DisableAbleCommandHandler("decide", decide)
+SLAP_HANDLER = DisableAbleCommandHandler("slap", slap)
+ALEXA_HANDLER = DisableAbleCommandHandler("alexa", alexa)
+
+dispatcher.add_handler(GOODMORNING_HANDLER)
+dispatcher.add_handler(ASADALI_HANDLER)
+dispatcher.add_handler(GOODNIGHT_HANDLER)
+dispatcher.add_handler(INSULT_HANDLER)
+dispatcher.add_handler(ABUSE_HANDLER)
+dispatcher.add_handler(GBAM_HANDLER)
+dispatcher.add_handler(GBUN_HANDLER)
+dispatcher.add_handler(PAT_HANDLER)
+dispatcher.add_handler(DECIDE_HANDLER)
+dispatcher.add_handler(JUDGE_HANDLER)
+dispatcher.add_handler(SLAP_HANDLER)
+dispatcher.add_handler(ALEXA_HANDLER)
+
+dispatcher.add_handler(TRUTH_HANDLER)
+dispatcher.add_handler(REPO_HANDLER)
+dispatcher.add_handler(DARE_HANDLER)
 
 
-def roll(update: Update, context: CallbackContext):
-    update.message.reply_text(random.choice(range(1, 7)))
 
-
-def shout(update: Update, context: CallbackContext):
-    args = context.args
-    text = " ".join(args)
-    result = []
-    result.append(" ".join(list(text)))
-    for pos, symbol in enumerate(text[1:]):
-        result.append(symbol + " " + "  " * pos + symbol)
-    result = list("\n".join(result))
-    result[0] = text[0]
-    result = "".join(result)
-    msg = "```\n" + result + "```"
-    return update.effective_message.reply_text(msg, parse_mode="MARKDOWN")
-
-
-def toss(update: Update, context: CallbackContext):
-    update.message.reply_text(random.choice(fun_strings.TOSS))
-
-
-def shrug(update: Update, context: CallbackContext):
-    msg = update.effective_message
-    reply_text = (
-        msg.reply_to_message.reply_text if msg.reply_to_message else msg.reply_text
-    )
-    reply_text(r"¯\_(ツ)_/¯")
-
-
-def bluetext(update: Update, context: CallbackContext):
-    msg = update.effective_message
-    reply_text = (
-        msg.reply_to_message.reply_text if msg.reply_to_message else msg.reply_text
-    )
-    reply_text(
-        "/BLUE /TEXT\n/MUST /CLICK\n/I /AM /A /STUPID /ANIMAL /THAT /IS /ATTRACTED /TO /COLORS",
-    )
-
-
-def rlg(update: Update, context: CallbackContext):
-    eyes = random.choice(fun_strings.EYES)
-    mouth = random.choice(fun_strings.MOUTHS)
-    ears = random.choice(fun_strings.EARS)
-
-    if len(eyes) == 2:
-        repl = ears[0] + eyes[0] + mouth[0] + eyes[1] + ears[1]
-    else:
-        repl = ears[0] + eyes[0] + mouth[0] + eyes[0] + ears[1]
-    update.message.reply_text(repl)
-
-
-def decide(update: Update, context: CallbackContext):
-    reply_text = (
-        update.effective_message.reply_to_message.reply_text
-        if update.effective_message.reply_to_message
-        else update.effective_message.reply_text
-    )
-    reply_text(random.choice(fun_strings.DECIDE))
-
-
-def eightball(update: Update, context: CallbackContext):
-    reply_text = (
-        update.effective_message.reply_to_message.reply_text
-        if update.effective_message.reply_to_message
-        else update.effective_message.reply_text
-    )
-    reply_text(random.choice(fun_strings.EIGHTBALL))
-
-
-def table(update: Update, context: CallbackContext):
-    reply_text = (
-        update.effective_message.reply_to_message.reply_text
-        if update.effective_message.reply_to_message
-        else update.effective_message.reply_text
-    )
-    reply_text(random.choice(fun_strings.TABLE))
-
-
-normiefont = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-]
-weebyfont = [
-    "卂",
-    "乃",
-    "匚",
-    "刀",
-    "乇",
-    "下",
-    "厶",
-    "卄",
-    "工",
-    "丁",
-    "长",
-    "乚",
-    "从",
-    "𠘨",
-    "口",
-    "尸",
-    "㔿",
-    "尺",
-    "丂",
-    "丅",
-    "凵",
-    "リ",
-    "山",
-    "乂",
-    "丫",
-    "乙",
-]
-
-
-def weebify(update: Update, context: CallbackContext):
-    args = context.args
-    message = update.effective_message
-    string = ""
-
-    if message.reply_to_message:
-        string = message.reply_to_message.text.lower().replace(" ", "  ")
-
-    if args:
-        string = "  ".join(args).lower()
-
-    if not string:
-        message.reply_text("Usage is `/weebify <text>`", parse_mode=ParseMode.MARKDOWN)
-        return
-
-    for normiecharacter in string:
-        if normiecharacter in normiefont:
-            weebycharacter = weebyfont[normiefont.index(normiecharacter)]
-            string = string.replace(normiecharacter, weebycharacter)
-
-    if message.reply_to_message:
-        message.reply_to_message.reply_text(string)
-    else:
-        message.reply_text(string)
-
+__mod_name__ = "💞 ғᴜɴ"
 
 __help__ = """
-❂ /runs*:* reply a random string from an array of replies
-❂ /slap*:* slap a user, or get slapped if not a reply
-❂ /shrug*:* get shrug XD
-❂ /table*:* get flip/unflip :v
-❂ /decide*:* Randomly answers yes/no/maybe
-❂ /toss*:* Tosses A coin
-❂ /bluetext*:* check urself :V
-❂ /roll*:* Roll a dice
-❂ /rlg*:* Join ears,nose,mouth and create an emo ;-;
-❂ /shout <keyword>*:* write anything you want to give loud shout
-❂ /weebify <text>*:* returns a weebified text
-❂ /sanitize*:* always use this before /pat or any contact
-❂ /pat*:* pats a user, or get patted
-❂ /8ball*:* predicts using 8ball method
-
-- Here is the help for the Styletext module:
-
-❂ /weebify <text>: weebify your text!
-❂ /bubble <text>: bubble your text!
-❂ /fbubble <text>: bubble-filled your text!
-❂ /square <text>: square your text!
-❂ /fsquare <text>: square-filled your text!
-❂ /blue <text>: bluify your text!
-❂ /latin <text>: latinify your text!
-❂ /lined <text>: lined your text!
-
-
- Sᴏᴍᴇ ɢᴀᴍᴇS ᴄᴏᴍᴍᴀɴᴅS
-
-❂ /dice - Dice 🎲
-❂/dart - Dart 🎯
-❂/basket - Basket Ball 🏀
-❂/bowling - Bowling Ball 🎳
-❂/football - Football ⚽
-❂/slot - Spin slot machine 
+=>> *Fun Module all cmd are given below
+ /alexa, /slap, /decide, /judge, /abuse, /insult, /truth, /dare, /gbam, /pat, /gbun
 """
-
-SANITIZE_HANDLER = DisableAbleCommandHandler("sanitize", sanitize, run_async=True)
-RUNS_HANDLER = DisableAbleCommandHandler("runs", runs, run_async=True)
-SLAP_HANDLER = DisableAbleCommandHandler("slap", slap, run_async=True)
-PAT_HANDLER = DisableAbleCommandHandler("pat", pat, run_async=True)
-ROLL_HANDLER = DisableAbleCommandHandler("roll", roll, run_async=True)
-TOSS_HANDLER = DisableAbleCommandHandler("toss", toss, run_async=True)
-SHRUG_HANDLER = DisableAbleCommandHandler("shrug", shrug, run_async=True)
-BLUETEXT_HANDLER = DisableAbleCommandHandler("bluetext", bluetext, run_async=True)
-RLG_HANDLER = DisableAbleCommandHandler("rlg", rlg, run_async=True)
-DECIDE_HANDLER = DisableAbleCommandHandler("decide", decide, run_async=True)
-EIGHTBALL_HANDLER = DisableAbleCommandHandler("8ball", eightball, run_async=True)
-TABLE_HANDLER = DisableAbleCommandHandler("table", table, run_async=True)
-SHOUT_HANDLER = DisableAbleCommandHandler("shout", shout, run_async=True)
-WEEBIFY_HANDLER = DisableAbleCommandHandler("weebify", weebify, run_async=True)
-
-dispatcher.add_handler(WEEBIFY_HANDLER)
-dispatcher.add_handler(SHOUT_HANDLER)
-dispatcher.add_handler(SANITIZE_HANDLER)
-dispatcher.add_handler(RUNS_HANDLER)
-dispatcher.add_handler(SLAP_HANDLER)
-dispatcher.add_handler(PAT_HANDLER)
-dispatcher.add_handler(ROLL_HANDLER)
-dispatcher.add_handler(TOSS_HANDLER)
-dispatcher.add_handler(SHRUG_HANDLER)
-dispatcher.add_handler(BLUETEXT_HANDLER)
-dispatcher.add_handler(RLG_HANDLER)
-dispatcher.add_handler(DECIDE_HANDLER)
-dispatcher.add_handler(EIGHTBALL_HANDLER)
-dispatcher.add_handler(TABLE_HANDLER)
-
-__mod_name__ = "「FUN」"
-__command_list__ = [
-    "runs",
-    "slap",
-    "roll",
-    "toss",
-    "shrug",
-    "bluetext",
-    "rlg",
-    "decide",
-    "table",
-    "pat",
-    "sanitize",
-    "shout",
-    "weebify",
-    "8ball",
-]
-__handlers__ = [
-    RUNS_HANDLER,
-    SLAP_HANDLER,
-    PAT_HANDLER,
-    ROLL_HANDLER,
-    TOSS_HANDLER,
-    SHRUG_HANDLER,
-    BLUETEXT_HANDLER,
-    RLG_HANDLER,
-    DECIDE_HANDLER,
-    TABLE_HANDLER,
-    SANITIZE_HANDLER,
-    SHOUT_HANDLER,
-    WEEBIFY_HANDLER,
-    EIGHTBALL_HANDLER,
-]
