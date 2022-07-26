ca23b0b97d11dd1ad5c64dff67a463bbc278d978
Roninopp <99870677+Roninopp@users.noreply.github.com>
2022-07-26 21:59:27 +0530

    Update fun_strings.py

--- TGN/modules/fun_strings.py
+++ TGN/modules/fun_strings.py
@@ -44,14 +44,14 @@ GBAM_REASON = (
 
 DECIDE_HANDLER = ("𝗬𝗲𝘀 ☑️", "𝗜𝘁𝘁𝘂 🤏 𝗦𝗮", "𝗡𝗼 🙅‍♀️")
 
-ALEXA_HANDLER = (
+fantastic_HANDLER = (
     "𝗗𝗼𝗻'𝘁 tag me on this 𝘀𝘁𝘂𝗽𝗶𝗱 user",
     "Only my love 𝗔𝘀𝗮𝗱 can tag me",
     "How dare you to 𝘁𝗮𝗴 me on this user",
     "Any 𝗽𝗿𝗼𝗯𝗹𝗲𝗺 viro 😜",
 )
 
-ALEXA_STRINGS = ("𝗛𝗲𝗹𝗹𝗼 user how are you & how may i 𝗵𝗲𝗹𝗽 you")
+fantastic_STRINGS = ("𝗛𝗲𝗹𝗹𝗼 user how are you & how may i 𝗵𝗲𝗹𝗽 you")
 
 WISH = (
     "*hey! (first)*\n*your Wish Possible to 25%😟*",
