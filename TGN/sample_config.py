4d07ecd0d9858bc829669cde1b9bdf1d1a8e2b4a
VIVEK <99870677+Roninopp@users.noreply.github.com>
2022-12-15 21:22:46 +0530

    Update sample_config.py

--- TGN/sample_config.py
+++ TGN/sample_config.py
@@ -14,12 +14,12 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
+    API_ID = 7217645  # integer value, dont use ""
+    API_HASH = "78ba6352dd5cdc166fdef5aa84ba7c67"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 1606221784  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = ""
-    SUPPORT_CHAT = ""  # Your own group for support, do not add the @
+    SUPPORT_CHAT = "Ronin_Fighters_Fd"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001432609692
     )  # Prints any new group the bot is added to, prints just the name and ID.
