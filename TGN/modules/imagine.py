6b370ec03d65c73097cc44dd930ef66ad89e0d1e
Md Noor <75603153+NoorXd786@users.noreply.github.com>
2023-03-26 21:46:31 +0530

    Update imagine.py

--- TGN/modules/imagine.py
+++ TGN/modules/imagine.py
@@ -1,72 +1,58 @@
-"""
-DOCUMENTATION:
-
-Dependencies:
-    - replicate
-    - pyrogram
-    
-How to get the API TOKEN?
-    - You can get the API token from `https://replicate.com/account`. After that add the token in your environment variables as `AI_ART_TOKEN`.
-
-How to use this module?
-    - Simply paste this module in your module folder, but first check if there is any import errors.
-
-Can I edit the model?
-    - Yeah, totally. BUt first check the documentation.
-    
-Reference:
-    - `https://docs.pyrogram.org`
-    - `https://replicate.com/docs`
-    
-Note: Please check `https://replicate.com/pricing` before using this module.
-
-        ~github.com/SOME-1HING
-"""
-
-import os
-import re
-import replicate
-
-from replicate.exceptions import ModelError
-from pyrogram.types import Message
 from pyrogram import Client, filters
+from pyrogram.types import *
+import os
+import json
+import requests
+import io
+import random
+from PIL import Image, PngImagePlugin
+import base64
+
 from TGN import pbot as app
 
-# Initialize the replicate client with the API token
-rp = replicate.Client(api_token=os.getenv("AI_ART_TOKEN", None))
+@app.on_message(filters.command(["draw"]))
+def draw(client, message):
+    msgs = message.text.split(' ', 1)
+    if len(msgs) == 1:
+        message.reply_text("Format : /draw < text to anime image >")
+        return
+    msg = msgs[1]
 
-# Get the model and version
-model = rp.models.get("stability-ai/stable-diffusion")
-version = model.versions.get(
-    "db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf"
-)
+    K = message.reply_text("Please Wait 10-15 Second")
 
+    payload = {"prompt": msg}
 
-# Define the command handler
-@app.on_message(filters.command("draw"))
-async def imagine(app: Client, message: Message):
-    # Check if the prompt is provided
-    if len(message.text.split()) < 2:
-        return await message.reply_text("Provide Prompt.")
+    r = requests.post(url=f'{SD_URL}/sdapi/v1/txt2img', json=payload).json()
 
-    # Extract the prompt from the command
-    prompt = re.sub(r"\b" + "/draw" + r"\b", "", message.text)
+    chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    chars1 = "1234564890"
+    gen1 = random.choice(chars)
+    gen2 = random.choice(chars)
+    gen3 = random.choice(chars1)
+    gen4 = random.choice(chars)
+    gen5 = random.choice(chars)
+    gen6 = random.choice(chars)
+    gen7 = random.choice(chars1)
+    gen8 = random.choice(chars)
+    gen9 = random.choice(chars)
+    gen10 = random.choice(chars1)
+    word = f"{message.from_user.id}-MOE{gen1}{gen2}{gen3}{gen4}{gen5}{gen6}{gen7}{gen8}{gen9}{gen10}"
 
-    # Show "Please wait" message
-    msg = await message.reply_text("Please wait.")
+    for i in r['images']:
+        image = Image.open(io.BytesIO(base64.b64decode(i.split(",", 1)[0])))
 
-    # Generate the image from the prompt using the model
-    try:
-        image = version.predict(prompt=prompt)[0]
-    except ModelError:
-        return await message.reply_text(
-            "NSFW content detected. Try running it again, or try a different prompt."
+        png_payload = {"image": "data:image/png;base64," + i}
+        response2 = requests.post(url=f'{url}/sdapi/v1/png-info',
+                                  json=png_payload)
+
+        pnginfo = PngImagePlugin.PngInfo()
+        pnginfo.add_text("parameters", response2.json().get("info"))
+        image.save(f'{word}.png', pnginfo=pnginfo)
+
+        message.reply_photo(
+            photo=f"{word}.png",
+            caption=
+            f"Prompt - **{msg}**\n **[{message.from_user.first_name}-Kun](tg://user?id={message.from_user.id})**\n Join @WaifuAiSupport"
         )
-
-    # Reply to the message with the generated image
-    await message.reply_photo(image)
-
-    # Delete the "Please wait" message
-    await msg.delete()
-
-    return
+        os.remove(f"{word}.png")
+        K.delete()
