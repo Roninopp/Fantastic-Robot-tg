e53232b6cca641e89395ed2ed127a0dc6b1b1b09
VIVEK <99870677+Roninopp@users.noreply.github.com>
2023-04-15 23:45:15 +0530

    Update ask.py

--- TGN/modules/ask.py
+++ TGN/modules/ask.py
@@ -4,7 +4,7 @@ import time
 import os 
 from pyrogram import filters
 
-openai.api_key = os.environ.get("OPENAI_TOKEN", "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")
+openai.api_key = os.environ.get("OPENAI_TOKEN")
 
 async def gen(prompt):
     #Fixed By Wolf :)
