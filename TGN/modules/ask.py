5790bf92be1f71532a61ac4329f7dfb8f1bd9c76
VIVEK <99870677+Roninopp@users.noreply.github.com>
2023-04-28 10:26:38 +0530

    Update ask.py

--- TGN/modules/ask.py
+++ TGN/modules/ask.py
@@ -10,10 +10,10 @@ async def generate_response(prompt):
     response = openai.Completion.create(
         engine="davinci",
         prompt=prompt,
-        max_tokens=1024,
+        max_tokens=100,
         n=1,
         stop=None,
-        temperature=0.7,
+        temperature=0.5,
     )
     return response.choices[0].text.strip()
 
