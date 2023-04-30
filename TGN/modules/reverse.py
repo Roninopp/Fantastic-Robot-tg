8811424aca2e961ee4d9c38e8bbaedd2f18d07d6
Priyansh #CrazyBoy <106391059+PriyanshGoswami@users.noreply.github.com>
2023-04-30 10:50:35 +0530

    Update reverse.py

--- TGN/modules/reverse.py
+++ TGN/modules/reverse.py
@@ -1,59 +1,68 @@
-import os
-import json
-import requests
-from telegram.ext import CommandHandler, run_async
+# Author: Sasta Dev || https://t.me/SastaDev
+# Author's github: https://github.com/SastaDev
+# Created on: Saturday, 29 April, 2023.
+# Written in python using pyrogram.
+# This python script only supports pyrogram versions lower than v2.0.
+# © Sasta Dev ~ 2023.
 
-from TGN import dispatcher
-from TGN.modules.disable import DisableAbleCommandHandler
+import uuid
+from pyrogram import Client, filters
+from pyrogram.types import (InlineKeyboardMarkup, InlineKeyboardButton, Message)
+import httpx
 
-api_key = "blue-api-testing"
-url = 'https://blue-api.vercel.app/reverse/'
+from TGN import pbot
 
+API_URL = 'https://sasta.tk/google_reverse'
 
-def reverse(update, context):
-    message = update.effective_message
-    chat_id = update.effective_chat.id
-    rtmid = message.message_id
+class STRINGS:
+    REPLY_TO_MEDIA = 'Reply to a message having photo, sticker or document.'
+    THESE_MEDIA_TYPES_ONLY = 'Only <b>photo</b>, <b>sticker</b> and <b>document</b> media types are allowed.'
+    GIF_NOT_SUPPORTED = 'GIF reverse is currently not available.'
+    DOWNLOADING_MEDIA = '<b>• Downloading media...</b>'
+    UPLOADING_MEDIA = '<b>• Uploading media...</b>'
+    API_ERROR = '<b>An API Error occured while requesting:</b>:\n{}'
+    SUPPORT_CHAT = '<b>Support Chat:</b> @HelpSupportChat'
+    REVERSE_RESULT = '''
+<b>Search Keyword:</b> <code>{}</code>
+<b>Results link:</b> <a href='{}'>Link</a>.
+<b>Credits:</b> @SastaDev
+    '''
 
-    reply = message.reply_to_message
+COMMANDS = ['reverse', 'grs', 'pp']
 
-    if reply:
-        if reply.sticker:
-            file_id = reply.sticker.file_id
-            new_id = reply.sticker.file_unique_id
-        elif reply.photo:
-            file_id = reply.photo[-1].file_id
-            new_id = reply.photo[-1].file_unique_id
-        else:
-            message.reply_text("Reply To An Image Or Sticker To Lookup!")
-            return
-
-        file_path = os.path.join("temp", f"{new_id}.jpg")
-        file_obj = context.bot.get_file(file_id)
-        file_url = file_obj.file_path
-
-    else:
-        message.reply_text(
-            "Please Reply To A Sticker, Or An Image To Search It!"
-        )
+@pbot.on_message(filters.command(COMMANDS))
+async def on_reverse(client: Client, message: Message) -> Message:
+    if not message.reply_to_message or not message.reply_to_message.media:
+        await message.reply(STRINGS.REPLY_TO_MEDIA)
         return
-
-    try:
-        params = {'api_token': api_key, 'image_url': file_url}
-        response = requests.get(url, params=params)
-        if response.status_code  == 200:
-            message.reply_text(response.text)
-        else:
-            message.reply_text("Cant find anything!!")
-            print(response.text)
-    except Exception as e:
-        print(e)
-        
-        
-REVERSE_HANDLER = DisableAbleCommandHandler(
-    ["reverse", "grs", "p", "pp"],
-    reverse,
-    run_async=True,
-)
-
-dispatcher.add_handler(REVERSE_HANDLER)
+    media_type = message.reply_to_message.media
+    if media_type not in ('photo', 'sticker', 'document'):
+        if media_type == 'animation':
+            await message.reply(STRINGS.GIF_NOT_SUPPORTED)
+            return
+        await message.reply(STRINGS.THESE_MEDIA_TYPES_ONLY)
+        return
+    status_msg = await message.reply(STRINGS.DOWNLOADING_MEDIA)
+    file_path = f'downloads/{uuid.uuid4()}'
+    await message.reply_to_message.download(file_path)
+    await status_msg.edit(STRINGS.UPLOADING_MEDIA)
+    async with httpx.AsyncClient(timeout=30) as async_client:
+        with open(file_path, 'rb') as file:
+            files = {'file': file}
+            response = await async_client.post(API_URL, files=files)
+        try:
+            response_json = response.json()
+        except:
+            await message.reply(STRINGS.API_ERROR.format(response.text) + '\n\n' +  STRINGS.SUPPORT_CHAT)
+            return
+        if response.status_code != 200:
+            await message.reply(STRINGS.API_ERROR.format(response_json['error']) + '\n\n' + STRINGS.SUPPORT_CHAT)
+            return
+        search_keyword = response_json['keyword']
+        url = response_json['url']
+    text = STRINGS.REVERSE_RESULT.format(search_keyword, url)
+    reply_markup = [
+        [InlineKeyboardButton('Open Link', url=url)]
+        ]
+    await message.reply(text, reply_markup=InlineKeyboardMarkup(reply_markup))
+    await status_msg.delete()
