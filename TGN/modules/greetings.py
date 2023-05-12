fae3f9795e78cffb461a8a0bea3b816ce2277788
definitelynotchirag <chiragdave1888@gmail.com>
2023-05-12 17:50:19 +0000

    error rem

--- TGN/modules/greetings.py
+++ TGN/modules/greetings.py
@@ -494,7 +494,7 @@ def left_member(update: Update, context: CallbackContext):
         # Clean service welcome
         if cleanserv:
             try:
-                NEKO_PTB.bot.delete_message(chat.id, update.message.message_id)
+                TGN_PTB.bot.delete_message(chat.id, update.message.message_id)
             except BadRequest:
                 pass
             reply = False
@@ -998,7 +998,7 @@ WELC_HELP_TXT = (
     "Welcome messages also support markdown, so you can make any elements bold/italic/code/links. "
     "Buttons are also supported, so you can make your welcomes look awesome with some nice intro "
     "buttons.\n"
-    f"To create a button linking to your rules, use this: `[Rules](buttonurl://t.me/{NEKO_PTB.bot.username}?start=group_id)`. "
+    f"To create a button linking to your rules, use this: `[Rules](buttonurl://t.me/{TGN_PTB.bot.username}?start=group_id)`. "
     "Simply replace `group_id` with your group's id, which can be obtained via /id, and you're good to "
     "go. Note that group ids are usually preceded by a `-` sign; this is required, so please don't "
     "remove it.\n"
