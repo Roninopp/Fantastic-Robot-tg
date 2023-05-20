8ec73538accc45e46668c776784363b648ecdfa6
Nya Nekko <122774834+NyaNekko@users.noreply.github.com>
2023-05-21 00:31:14 +0530

    Create botstats.py

---
+++ TGN/modules/botstats.py
@@ -0,0 +1,12 @@
+from pyrogram import *
+from pyrogram.types import *
+
+from TGN import pbot as app
+
+from TGN.__main__ import STATS 
+
+@app.on_message(filters.command("bstats"), group=69)
+async def botstats(_, message):
+    text = "Bot Stats\n"
+    await message.reply_text(text + "\n".join([mod.stats() for mod in STATS]))
+
