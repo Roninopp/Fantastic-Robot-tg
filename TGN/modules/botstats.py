8956f737d98ef0c6d38826deb93fe78bdf6255c5
VIVEK <99870677+Roninopp@users.noreply.github.com>
2023-05-24 17:53:47 +0530

    Update botstats.py

--- TGN/modules/botstats.py
+++ TGN/modules/botstats.py
@@ -7,6 +7,6 @@ from TGN.__main__ import STATS
 
 @app.on_message(filters.command("bstats"), group=69)
 async def botstats(_, message):
-    text = "Bot Stats\n"
+    text = "Fantastic Bot Stats\n"
     await message.reply_text(text + "\n".join([mod.__stats__() for mod in STATS]))
 
