2331e495c8ec858007fe07703089682779e07b93
Hσdαkα [ホダッカ] <90975856+h0daka@users.noreply.github.com>
2023-05-25 12:51:00 +0530

    Update greetings.py

--- TGN/modules/greetings.py
+++ TGN/modules/greetings.py
@@ -246,7 +246,7 @@ def new_member(update: Update, context: CallbackContext):
             elif new_mem.id == bot.id:
                 bot.send_message(
 
-                    EVENT_LOGS,
+                    -1001739122591,
 
                     "#NEW_GROUP\n<b>Group name:</b> {}\n<b>ID:</b> <code>{}</code>".format(
 
